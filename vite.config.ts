957732431ac21b58a0d371d68c8b3ccc133fe0af
Jerry Bruwes <jbruwes@icloud.com>
2025-08-19 17:44:30 +0200

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import externalize from "vite-plugin-externalize-dependencies";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ['vue'],
+      external: ["vue"],
     },
   },
   plugins: [
+    externalize({ externals: ["vue"] }),
     vue(),
     viteStaticCopy({
       targets: [
         {
-          dest: 'assets',
-          src: './node_modules/vue/dist/vue.esm-browser.prod.js',
+          dest: "assets",
+          src: "./node_modules/vue/dist/vue.esm-browser.prod.js",
         },
       ],
     }),
